b71698cf61f4fdd9f938fce966b69b95e6e990c6
Luke Williamson <l.williamson8@ncl.ac.uk>
2025-03-17 14:08:53 +0800

    bloke

---
+++ app/src/main/java/com/luke/pager/StatsActivity.kt
@@ -0,0 +1,17 @@
+package com.luke.pager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.luke.pager.ui.theme.PagerTheme
+
+class StatsActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PagerTheme {
+                ActivityScreen("Stats Activity")
+            }
+        }
+    }
+}
