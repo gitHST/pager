07935efcfb199c4028bd3e8b852be551ebed09bd
Luke Williamson <l.williamson8@ncl.ac.uk>
2025-04-02 22:20:49 +0800

    Add database and DAOs and entities

---
+++ app/src/main/java/com/luke/pager/data/dao/BookDao.kt
@@ -0,0 +1,28 @@
+package com.luke.pager.data.dao
+
+import BookEntity
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface BookDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertBook(book: BookEntity)
+
+    @Query("SELECT * FROM books WHERE id = :id")
+    suspend fun getBookById(id: Int): BookEntity?
+
+    @Query("SELECT * FROM books")
+    fun getAllBooks(): Flow<List<BookEntity>>
+
+    @Update
+    suspend fun updateBook(book: BookEntity)
+
+    @Delete
+    suspend fun deleteBook(book: BookEntity)
+}
---
+++ app/src/main/java/com/luke/pager/data/dao/ReviewDao.kt
@@ -0,0 +1,25 @@
+package com.luke.pager.data.dao
+
+import ReviewEntity
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ReviewDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertReview(review: ReviewEntity)
+
+    @Query("SELECT * FROM reviews WHERE book_id = :bookId")
+    fun getReviewsForBook(bookId: Int): Flow<List<ReviewEntity>>
+
+    @Update
+    suspend fun updateReview(review: ReviewEntity)
+
+    @Delete
+    suspend fun deleteReview(review: ReviewEntity)
+}
