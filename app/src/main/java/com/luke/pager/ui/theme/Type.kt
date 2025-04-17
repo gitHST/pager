56df074172b4735916b518f5e03e5855c27a3228
Luke Williamson <l.williamson8@ncl.ac.uk>
2025-04-17 18:35:25 +0800

    Added new default font

--- app/src/main/java/com/luke/pager/ui/theme/Type.kt
+++ app/src/main/java/com/luke/pager/ui/theme/Type.kt
@@ -4,31 +4,45 @@ import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.unit.sp
+import com.luke.pager.R
 
-// Set of Material typography styles to start with
+// 1. Define the Google Font Provider
+private val gentiumProvider = GoogleFont.Provider(
+    providerAuthority = "com.google.android.gms.fonts",
+    providerPackage = "com.google.android.gms",
+    certificates = R.array.com_google_android_gms_fonts_certs
+)
+
+// 2. Create the FontFamily using Inter
+private val GentiumFontFamily = FontFamily(
+    Font(googleFont = GoogleFont("Gentium Book Plus"), fontProvider = gentiumProvider, weight = FontWeight.Normal),
+    Font(googleFont = GoogleFont("Gentium Book Plus"), fontProvider = gentiumProvider, weight = FontWeight.Medium),
+    Font(googleFont = GoogleFont("Gentium Book Plus"), fontProvider = gentiumProvider, weight = FontWeight.Bold),
+)
+
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = GentiumFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = GentiumFontFamily,
+        fontWeight = FontWeight.Bold,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = GentiumFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     )
-    */
 )
