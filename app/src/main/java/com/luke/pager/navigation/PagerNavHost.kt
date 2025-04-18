4ef50bd04c20278449603da8450955d96b90fe42
Luke Williamson <l.williamson8@ncl.ac.uk>
2025-04-18 20:45:09 +0800

    Removed back button from review and added swipe navigation

---
+++ app/src/main/java/com/luke/pager/navigation/PagerNavHost.kt
@@ -0,0 +1,68 @@
+package com.luke.pager.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.luke.pager.data.viewmodel.BookViewModel
+import com.luke.pager.data.viewmodel.ReviewViewModel
+import com.luke.pager.screens.ActivityScreen
+import com.luke.pager.screens.AddScreen
+import com.luke.pager.screens.DiaryScreen
+import com.luke.pager.screens.QuotesScreen
+import com.luke.pager.screens.ReviewScreen
+import com.luke.pager.screens.SearchScreen
+
+
+@Composable
+fun PagerNavHost(
+    navController: NavHostController,
+    bookViewModel: BookViewModel,
+    reviewViewModel: ReviewViewModel
+) {
+    val navItems = listOf(
+        NavItem("activity", "Activity"),
+        NavItem("diary", "Diary"),
+        NavItem("plus", "+"),
+        NavItem("search", "Search"),
+        NavItem("quotes", "Quotes")
+    )
+
+    val currentRoute by navController.currentBackStackEntryAsState()
+
+    NavHost(navController, startDestination = "plus") {
+        composable("activity") {
+            SwipeToNavigate(navController, currentRoute?.destination?.route.orEmpty(), navItems) {
+                ActivityScreen(bookViewModel)
+            }
+        }
+        composable("diary") {
+            SwipeToNavigate(navController, currentRoute?.destination?.route.orEmpty(), navItems) {
+                DiaryScreen(navController, bookViewModel)
+            }
+        }
+        composable("plus") {
+            SwipeToNavigate(navController, currentRoute?.destination?.route.orEmpty(), navItems) {
+                AddScreen(bookViewModel, reviewViewModel)
+            }
+        }
+        composable("search") {
+            SwipeToNavigate(navController, currentRoute?.destination?.route.orEmpty(), navItems) {
+                SearchScreen(bookViewModel)
+            }
+        }
+        composable("quotes") {
+            SwipeToNavigate(navController, currentRoute?.destination?.route.orEmpty(), navItems) {
+                QuotesScreen(bookViewModel)
+            }
+        }
+        composable("review_screen/{reviewId}") { backStackEntry ->
+            val reviewId = backStackEntry.arguments?.getString("reviewId")?.toLongOrNull() ?: 0L
+            val reviews by bookViewModel.allReviews.collectAsState()
+            ReviewScreen(navController = navController, reviewId = reviewId, reviews = reviews)
+        }
+    }
+}
