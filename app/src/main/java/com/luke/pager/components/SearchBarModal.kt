297fbbfab61e48e435f3726ba9b4400cf95f3377
Luke Williamson <l.williamson8@ncl.ac.uk>
2025-04-19 23:26:36 +0800

    Added loading wheel

--- app/src/main/java/com/luke/pager/components/SearchBarModal.kt
+++ app/src/main/java/com/luke/pager/components/SearchBarModal.kt
@@ -1,5 +1,6 @@
 package com.luke.pager.components
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -7,6 +8,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.BottomSheetDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
@@ -22,6 +24,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.luke.pager.network.apiresponse.OpenLibraryBook
@@ -52,23 +55,26 @@ fun SearchBookModal(
     }
 
     LaunchedEffect(searchQuery) {
-        searchJob?.cancel() // cancel the last search if still running
+        searchJob?.cancel()
 
         if (searchQuery.isBlank()) {
             books = emptyList()
+            isLoading = false
             return@LaunchedEffect
         }
 
         val currentQuery = searchQuery
 
         searchJob = scope.launch {
-            delay(150) // debounce duration
-
+            delay(500)
             if (currentQuery == searchQuery) {
+                isLoading = true
                 books = searchBooksSmart(currentQuery)
+                isLoading = false
             }
         }
     }
+
 
 
 
@@ -97,25 +103,34 @@ fun SearchBookModal(
                     containerColor = MaterialTheme.colorScheme.surfaceContainerLow
                 )
             ) {
-                if (books.isNotEmpty()) {
-                    Column(modifier = Modifier.padding(8.dp)) {
-                        books.forEach { book ->
-                            Column(modifier = Modifier.padding(bottom = 8.dp)) {
-                                Text(
-                                    book.title,
-                                    style = MaterialTheme.typography.bodyLarge
-                                )
-                                book.author_name?.let {
-                                    Text(
-                                        it.joinToString(),
-                                        style = MaterialTheme.typography.bodySmall
-                                    )
+                when {
+                    isLoading -> {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(top = 100.dp),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            CircularProgressIndicator()
+                        }
+                    }
+
+                    books.isNotEmpty() -> {
+                        Column(modifier = Modifier.padding(8.dp)) {
+                            books.forEach { book ->
+                                Column(modifier = Modifier.padding(bottom = 8.dp)) {
+                                    Text(book.title, style = MaterialTheme.typography.bodyLarge)
+                                    book.author_name?.let {
+                                        Text(it.joinToString(), style = MaterialTheme.typography.bodySmall)
+                                    }
                                 }
                             }
                         }
                     }
-                } else if (searchQuery.isNotBlank()) {
-                    Text("No results", modifier = Modifier.padding(8.dp))
+
+                    searchQuery.isNotBlank() -> {
+                        Text("No results", modifier = Modifier.padding(8.dp))
+                    }
                 }
             }
 
